2f1221ad617cbe75854d661f2adbb2dfa0b25ad4
Rita Zerrizuela <zeta@widcket.com>
2020-09-16 20:24:22 -0300

    Bump swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "JWTDecode",
-            type: .dynamic,
             targets: ["JWTDecode"]),
     ],
     dependencies: [
